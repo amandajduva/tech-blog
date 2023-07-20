374e51d1c1858e298d98a97a01fa3cb5610fb1fe
Amanda Duva <amanda.j.duva@gmail.com>
2023-07-20 12:57:27 -0400

    updated models

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+module.exports = Comment;
